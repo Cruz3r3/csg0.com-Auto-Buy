667a8612545f7382bd3121e12f313801b6b1f9fc
Invisible <tomas0427aw@gmail.com>
2015-09-06 15:55:59 +0300

    Update auto-buy.js

--- auto-buy.js
+++ auto-buy.js
@@ -1,5 +1,6 @@
 (function() {
     alert("Auto Buy has started!");
-    $(".item[data-value='2']").css('color', 'white');
-    alert("Auto Buy has started!");
+    $(".item[data-value='2']").click(function() { 
+        alert("clicked");
+    });
 }).call(this);
