10b41a4d66b21efa7faa661e9345a77e078d883c
Invisible <tomas0427aw@gmail.com>
2015-09-20 12:14:11 +0300

    Update auto-buy.js

--- auto-buy.js
+++ auto-buy.js
@@ -1,5 +1,6 @@
 (function() {
-    $(".item[data-value='2']").click(function() { 
+    /*$(".item[data-value='2']").click(function() { 
         alert("clicked");
-    });
+    });*/
+    $('#checkout').append('<button id="checkout" class="btn btn-success">Take`Em all!</button>');
 }).call(this);
